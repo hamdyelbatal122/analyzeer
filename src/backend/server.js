aa275b58e630e104c59a34b224f559dd7b13bff9
GitSquared <GitSquared@users.noreply.github.com>
2018-06-24 17:35:50 +0200

    Access Deezer's API data from frontend

--- src/backend/server.js
+++ src/backend/server.js
@@ -43,47 +43,33 @@ async function loadStatics() {
         Object.defineProperty(global._static, name, {
             get: function() {
                 if (process.env.DEV_MODE === "ACTIVATED") {
-                    return fs.readFileSync("src/static"+file, {encoding: "utf-8"});
+                    return fs.readFileSync("src/static/"+file, {encoding: "utf-8"});
                 } else {
                     return this["_"+name];
                 }
             }
         });
     });
-    global._static.app = fs.readFileSync("src/webapp/index.html", {encoding: "utf-8"});
-    global._static.appbundle = fs.readFileSync("src/webapp/dist/bundle.js", {encoding: "utf-8"});
+    global._static._app = fs.readFileSync("src/webapp/index.html", {encoding: "utf-8"});
+    global._static._appbundle = fs.readFileSync("src/webapp/dist/bundle.js", {encoding: "utf-8"});
 }
 
-async function harvestAPI(code) {
-    let data = {};
-    let api = new DeezerAPIConnection();
-    api.connect(global._deezerapp, code).then(() => {
-        api.getUserInfo().then(u => {
-            data.user = u;
-            return api.getFavoriteTracks();
-        }).then(t => {
-            data.tracks = t;
-            return api.getTopTracks();
-        }).then(tp => {
-            data.top = tp;
-            return api.getLastHundred();
-        }).then(h => {
-            data.history = h;
-            return data;
-        }).catch(e => {
-            throw(e);
-        });
-    }).catch(e => {
-        throw(e);
-    });
+async function harvestAPI(api) {
+    return data = {
+        user: await api.getUserInfo(),
+        tracks: await api.getFavoriteTracks(),
+        top: await api.getTopTracks(),
+        history: await api.getLastHundred()
+    };
 }
 
 async function startServer() {
     await loadStatics();
 
     const {DeezerAPIConnection} = require("./classes/deezer-api-wrapper.class.js");
-    const express = require("express");
-    const app = express();
+    const app = require("express")();
+    const http = require("http").Server(app);
+    const io = require("socket.io")(http);
 
     app.get("/", (req, res) => {
         if (req.query.error_reason) {
@@ -98,8 +84,31 @@ async function startServer() {
     app.get("/bundle.js", (req, res) => {
         res.send(global._static.appbundle);
     });
+    app.get("/500", (req, res) => {
+        res.send(global._static.internerror);
+    });
 
-    app.listen(global._deezerapp.port);
+    io.on("connection", socket => {
+        let api = new DeezerAPIConnection();
+        socket.once("apiconnect", (code, res) => {
+            api.connect(global._deezerapp, code).then(() => {
+                res("200 OK");
+            }).catch(e => {
+                res(e);
+            });
+        });
+        socket.on("data request", res => {
+            harvestAPI(api).then(data => {
+                res(data);
+            }).catch(e => {
+                res("Error");
+            });
+        });
+    });
+
+    http.listen(global._deezerapp.port, () => {
+        console.log(`Listening @ ${global._deezerapp.url}:${global._deezerapp.port}`);
+    });
 }
 
 // Helpers
--- src/webapp/index.js
+++ src/webapp/index.js
@@ -1,2 +1,24 @@
-// halo
-document.body.append("hello?");
+import io from "socket.io-client";
+
+const socket = io();
+const code = window.location.search.slice(6);
+
+socket.emit("apiconnect", code, res => {
+    if (res === "200 OK") {
+        socket.emit("data request", res => {
+            if (res === "Error") {
+                socket.disconnect();
+                window.location.assign("/500");
+            } else {
+                render(res);
+            }
+        });
+    } else {
+        socket.disconnect();
+        window.location.assign("/500")
+    }
+});
+
+function render(data) {
+    document.body.append(JSON.stringify(data));
+}

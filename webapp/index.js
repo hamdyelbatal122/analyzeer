9ccd44096f1783e73384e4a7f98e7a993959a4aa
GitSquared <GitSquared@users.noreply.github.com>
2018-06-26 10:46:31 +0200

    :lipstick: smoooooth

--- webapp/index.js
+++ webapp/index.js
@@ -44,41 +44,41 @@ function render(data) {
     }
 
     window.root.innerHTML += `
-    <section id="user" class="wow fadeInUp">
+    <section id="user" class="wow fadeInLeft">
         <img src="${data.user.picture_medium}" alt="${data.user.name}'s photo'" />
         <h1>${data.user.name} <span>#${data.user.id}</span></h1>
         <h3>On Deezer since ${data.user.inscription_date}</h3>
         <a href="${data.user.link}" target="_blank">View on Deezer</a>
     </section>
-    <section id="account" class="wow fadeInUp yellow">
+    <section id="account" class="wow fadeInDown yellow" data-wow-delay="300ms">
         <h1>Analyzeer account</h1>
     </section>
-    <section id="credits" class="wow fadeInUp dark">
+    <section id="credits" class="wow fadeInRight dark" data-wow-delay="600ms">
         <h1>Credits</h1>
     </section>
-    <section id="graph1" class="wow fadeInUp blue">
-        <h1>Graph1</h1>
-    </section>
-    <section id="info1" class="wow fadeInUp">
+    <section id="info1" class="wow fadeInLeft" data-wow-delay="950ms">
         <h1>Info1</h1>
     </section>
-    <section id="stat1" class="wow fadeInUp pink">
+    <section id="graph1" class="wow fadeInUp blue" data-wow-delay="800ms">
+        <h1>Graph1</h1>
+    </section>
+    <section id="stat1" class="wow fadeInRight pink" data-wow-delay="1s">
         <h1>Stat1</h1>
     </section>
-    <section id="info2" class="wow fadeInUp">
-        <h1>Info2</h1>
-    </section>
-    <section id="stat2" class="wow fadeInUp pink">
+    <section id="stat2" class="wow fadeInUp pink" data-wow-delay="1200ms">
         <h1>Stat2</h1>
     </section>
-    <section id="stat3" class="wow fadeInUp pink">
+    <section id="info2" class="wow fadeInUp" data-wow-delay="1400ms">
+        <h1>Info2</h1>
+    </section>
+    <section id="stat3" class="wow fadeInRight pink" data-wow-delay="1600ms">
         <h1>Stat3</h1>
     </section>
-    <section id="graph2" class="wow fadeInUp blue">
-        <h1>Graph2</h1>
-    </section>
-    <section id="info3" class="wow fadeInUp">
+    <section id="info3" class="wow fadeInUp" data-wow-delay="1800ms">
         <h1>Info3</h1>
+    </section>
+    <section id="graph2" class="wow fadeInUp blue" data-wow-delay="2s">
+        <h1>Graph2</h1>
     </section>`;
 
     new WOW({live:false}).init();
